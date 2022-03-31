ac2411c56441edbdb52c031c4090327b3c0c5be3
vitoriadsouza <102767646+vitoriadsouza@users.noreply.github.com>
2022-03-31 18:58:58 -0300

    Adicionei atividade da mentoria.

---
+++ main.py
@@ -0,0 +1,9 @@
+c= int(input("digite a temperatura."))
+f= (c*9/5) + 32
+print(f)
+if c <= 15:
+    print("está frio.")
+if c >= 25:
+    print("está quente.")
+if c >=16 and c < 25:
+    print("está agradável.")
